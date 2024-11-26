0b3fc31f0a6aa0cc0732b84359ddd6cff53dc006
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2024-11-26 17:23:03 +0300

    Added a sidebar component rather than a a mere balde file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,11 @@
 
         <!-- Sidebar Drawer Content -->
         <div class="drawer-side">
-            <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+            <label for="my-drawer-2"  class="drawer-overlay"></label>
 
-            @include('layouts.sidebar')
+            <!-- @include('layouts.sidebar') -->
+            <livewire:components.sidebar />
+            
         </div>
     </div>
 
